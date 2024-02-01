02d64bec6c61e9b6db1424cc204dd7fe72ce3027
LarsPresta <lauri.monni@gmail.com>
2024-02-01 10:22:33 +0100

    game logic

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
@@ -9,14 +11,29 @@ const btnHold = document.querySelector('.btn--hold');
 
 score0El.textContent = 0;
 score1El.textContent = 0;
-
 diceEl.classList.add('hidden');
+
+const scores = [0, 0];
+let currentScore = 0;
+let activePlayer = 0;
 
 //Rolling dice functionality
 btnRoll.addEventListener('click', function () {
   // generate dice roll
   const dice = Math.trunc(Math.random() * 6) + 1;
 
+  //display dice roll
   diceEl.classList.remove('hidden');
   diceEl.src = `dice-${dice}.png`;
+
+  if (dice !== 1) {
+    currentScore += dice;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+  } else {
+    //Switch player
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    currentScore = 0;
+  }
 });
